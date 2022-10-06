a6f5acf3b87f26814493ef58224dc3aae8de44a5
Fred Lawrence <fred9702@gmail.com>
2022-10-06 20:24:14 +0100

    Commit connected to postgresql, black

--- ddc/app/views.py
+++ ddc/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class HitViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+
+    serializer_class = HitSerializer
+    queryset = Hit.objects.all()
